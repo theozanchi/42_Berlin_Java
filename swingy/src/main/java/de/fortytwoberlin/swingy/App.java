b3492388068c83cd626de45ad9ab0c0c8eb90d43
tzanchi <tzanchi@student.42berlin.de>
2024-08-09 18:35:21 +0200

    init of mavemn project

---
+++ swingy/src/main/java/de/fortytwoberlin/swingy/App.java
@@ -0,0 +1,13 @@
+package de.fortytwoberlin.swingy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
